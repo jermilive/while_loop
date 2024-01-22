ed64fbb0e8b513fdc3a029550c2a4d17e34ac0ac
Jeremiah Bates <jermilive@gmail.com>
2024-01-22 17:26:47 -0600

    update

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
+// The variable assignment makes 'n' equal to 0 'zero'
+
 var n = 0;
 
 while (n < 5) {
+  // 'n++' incriments the value 'n' by 1 each time the loop is run
   n++;
   console.log('n = ' + n);
 }
 
+// Do While Loop
 var i = 0;
 
 do {
@@ -16,12 +20,13 @@ do {
 //   console.log(event.clientY);
 // });
 
-// Shows cursor position
+// Shows cursor position in console
 document.addEventListener('mousemove', (event) => {
   console.log('event.clientX && event.clientY');
 });
 
 // Saved to look at later
+
 // const eleSvg = document.querySelector('svg');
 // let point = eleSvg.createSVGPoint();
 // var cursorpt = point.matrixTransform(eleSvg.getScreenCTM().inverse());
